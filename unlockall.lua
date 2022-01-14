36ec531da1ccce2c7a25218bf296c9a6995db9fa
Candorada <51519091+Candorada@users.noreply.github.com>
2022-01-14 18:51:29 +0100

    Create unlockall.lua

---
+++ unlockall.lua
@@ -0,0 +1,147 @@
+--what to touch
+local playerHead = game.Workspace.Candorada.HumanoidRootPart
+--loop
+for i, v in pairs(game:GetService("Workspace").Portals.UnlockParts.Zone2:GetDescendants()) do
+    if v.Name == "TouchInterest" then
+firetouchinterest(playerHead, v.Parent, 0)
+wait(0.1)
+firetouchinterest(playerHead, v.Parent, 1)
+break; 
+    end
+end
+for i, v in pairs(game:GetService("Workspace").Portals.UnlockParts.Zone3:GetDescendants()) do
+    if v.Name == "TouchInterest" then
+firetouchinterest(playerHead, v.Parent, 0)
+wait(0.1)
+firetouchinterest(playerHead, v.Parent, 1)
+break; 
+    end
+end
+for i, v in pairs(game:GetService("Workspace").Portals.UnlockParts.Zone4:GetDescendants()) do
+    if v.Name == "TouchInterest" then
+firetouchinterest(playerHead, v.Parent, 0)
+wait(0.1)
+firetouchinterest(playerHead, v.Parent, 1)
+break; 
+    end
+end
+for i, v in pairs(game:GetService("Workspace").Portals.UnlockParts.Zone5:GetDescendants()) do
+    if v.Name == "TouchInterest" then
+firetouchinterest(playerHead, v.Parent, 0)
+wait(0.1)
+firetouchinterest(playerHead, v.Parent, 1)
+break; 
+    end
+end
+for i, v in pairs(game:GetService("Workspace").Portals.UnlockParts.Zone6:GetDescendants()) do
+    if v.Name == "TouchInterest" then
+firetouchinterest(playerHead, v.Parent, 0)
+wait(0.1)
+firetouchinterest(playerHead, v.Parent, 1)
+break; 
+    end
+end
+for i, v in pairs(game:GetService("Workspace").Portals.UnlockParts.Zone7:GetDescendants()) do
+    if v.Name == "TouchInterest" then
+firetouchinterest(playerHead, v.Parent, 0)
+wait(0.1)
+firetouchinterest(playerHead, v.Parent, 1)
+break; 
+    end
+end
+for i, v in pairs(game:GetService("Workspace").Portals.UnlockParts.Zone8:GetDescendants()) do
+    if v.Name == "TouchInterest" then
+firetouchinterest(playerHead, v.Parent, 0)
+wait(0.1)
+firetouchinterest(playerHead, v.Parent, 1)
+break; 
+    end
+end
+for i, v in pairs(game:GetService("Workspace").Portals.UnlockParts.Zone9:GetDescendants()) do
+    if v.Name == "TouchInterest" then
+firetouchinterest(playerHead, v.Parent, 0)
+wait(0.1)
+firetouchinterest(playerHead, v.Parent, 1)
+break; 
+    end
+end
+for i, v in pairs(game:GetService("Workspace").Portals.UnlockParts.Zone10:GetDescendants()) do
+    if v.Name == "TouchInterest" then
+firetouchinterest(playerHead, v.Parent, 0)
+wait(0.1)
+firetouchinterest(playerHead, v.Parent, 1)
+break; 
+    end
+end
+for i, v in pairs(game:GetService("Workspace").Portals.UnlockParts.Zone11:GetDescendants()) do
+    if v.Name == "TouchInterest" then
+firetouchinterest(playerHead, v.Parent, 0)
+wait(0.1)
+firetouchinterest(playerHead, v.Parent, 1)
+break; 
+    end
+end
+for i, v in pairs(game:GetService("Workspace").Portals.UnlockParts.Zone12:GetDescendants()) do
+    if v.Name == "TouchInterest" then
+firetouchinterest(playerHead, v.Parent, 0)
+wait(0.1)
+firetouchinterest(playerHead, v.Parent, 1)
+break; 
+    end
+end
+for i, v in pairs(game:GetService("Workspace").Portals.UnlockParts.Zone13:GetDescendants()) do
+    if v.Name == "TouchInterest" then
+firetouchinterest(playerHead, v.Parent, 0)
+wait(0.1)
+firetouchinterest(playerHead, v.Parent, 1)
+break; 
+    end
+end
+for i, v in pairs(game:GetService("Workspace").Portals.UnlockParts.Zone14:GetDescendants()) do
+    if v.Name == "TouchInterest" then
+firetouchinterest(playerHead, v.Parent, 0)
+wait(0.1)
+firetouchinterest(playerHead, v.Parent, 1)
+break; 
+    end
+end
+for i, v in pairs(game:GetService("Workspace").Portals.UnlockParts.Zone15:GetDescendants()) do
+    if v.Name == "TouchInterest" then
+firetouchinterest(playerHead, v.Parent, 0)
+wait(0.1)
+firetouchinterest(playerHead, v.Parent, 1)
+break; 
+    end
+end
+for i, v in pairs(game:GetService("Workspace").Portals.UnlockParts.Zone16:GetDescendants()) do
+    if v.Name == "TouchInterest" then
+firetouchinterest(playerHead, v.Parent, 0)
+wait(0.1)
+firetouchinterest(playerHead, v.Parent, 1)
+break; 
+    end
+end
+for i, v in pairs(game:GetService("Workspace").Portals.UnlockParts.Zone17:GetDescendants()) do
+    if v.Name == "TouchInterest" then
+firetouchinterest(playerHead, v.Parent, 0)
+wait(0.1)
+firetouchinterest(playerHead, v.Parent, 1)
+break; 
+    end
+end
+for i, v in pairs(game:GetService("Workspace").Portals.UnlockParts.Zone18:GetDescendants()) do
+    if v.Name == "TouchInterest" then
+firetouchinterest(playerHead, v.Parent, 0)
+wait(0.1)
+firetouchinterest(playerHead, v.Parent, 1)
+break; 
+    end
+end
+for i, v in pairs(game:GetService("Workspace").Portals.UnlockParts.Zone19:GetDescendants()) do
+    if v.Name == "TouchInterest" then
+firetouchinterest(playerHead, v.Parent, 0)
+wait(0.1)
+firetouchinterest(playerHead, v.Parent, 1)
+break; 
+    end
+end
